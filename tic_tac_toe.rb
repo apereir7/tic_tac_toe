54a0558aced15c557c5dfe796fafce19b068ecb8
Anthony Pereira <pereiratony@rocektmail.com>
2022-04-13 20:31:08 -0500

    display grid

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,50 @@
 # frozen_string_literal: false
 
-matrix = [
-  [nil, nil, nil],
-  [nil, nil, nil],
-  [nil, nil, nil]
-]
+require 'pry-byebug'
 
-p matrix[0]
-p matrix[1]
-p matrix[2]
+# Display grid
+module Display
+end
+
+# player
+class Player
+  attr_accessor :pick
+
+  include Display
+
+  @@matrix = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+  ]
+
+  def grid
+    puts '     r1   r2    r3'
+    puts "c1  #{@@matrix[0]}"
+    puts "c2  #{@@matrix[1]}"
+    puts "c3  #{@@matrix[2]}"
+  end
+
+  def initialize(pick)
+    @pick = pick
+  end
+
+  def self.var
+    p @@matrix
+  end
+
+  def self.grid
+    puts '     r1   r2    r3'
+    puts "c1  #{@@matrix[0]}"
+    puts "c2  #{@@matrix[1]}"
+    puts "c3  #{@@matrix[2]}"
+  end
+end
+
+first_player = Player.new('X')
+
+second_player = Player.new('O')
+
+puts first_player.pick
+
+first_player.grid

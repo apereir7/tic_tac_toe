34ab419c6b8dcdc1874cb2aeb00e6464c9127ebb
Anthony Pereira <pereiratony@rocektmail.com>
2022-04-30 18:07:43 -0500

    update work

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: false
 
-require 'pry-byebug'
-
 # Display
 module Display
   # check individual row
@@ -10,13 +8,12 @@ module Display
   end
 
   # check horizantal row
-
-  def three_in_row?(array1, array2, array3)
-    row_check?(array1) || row_check?(array2) || row_check?(array3)
+  def horrizantal?(array)
+    row_check?(array[0..2]) || row_check?(array[3..5]) || row_check?(array[6..8])
   end
 
   # check vertical columm
-  def three_in_column?(array)
+  def column?(array)
     row_check?([array[0], array[3], array[6]]) ||
       row_check?([array[1], array[4], array[7]]) ||
       row_check?([array[3], array[5], array[8]])
@@ -30,16 +27,18 @@ module Display
     row_check?(diagnal1) || row_check?(diagnal2)
   end
 
-  def game?(array)
+  # tic tac toe, three in a row
+  def tictactoe?(array)
     # check the rows
-    three_in_row?(array[0..2], array[3..5], array[6..8]) ||
+    horrizantal?(array) ||
       # check the columns
-      three_in_column?(array) ||
+      column?(array) ||
       # check diagnal
       diagonally?(array)
   end
 
-  def grid(array)
+  # aesthetically pleasing display
+  def display_grid(array)
     puts "\n"
     puts array[0..2].to_s
     puts array[3..5].to_s
@@ -49,6 +48,35 @@ module Display
   def change_cell(num, array1, array2)
     array1[num - 1] = pick
     array2[num - 1] = pick
+  end
+
+  def take_turn(player1, player2)
+    if tictactoe?(player1.matrix_grid)
+      puts "#{player1.pick} wins"
+    elsif  tictactoe?(player2.matrix_grid)
+      puts "#{player2.pick} wins"
+    else
+      not_three_in_row()
+    end
+  end
+
+  def not_three_in_row()
+    puts "\nPick an empty cell player X\n"
+    num1 = gets.chomp
+    player1.change_cell(num1, player1.matrix_grid, player1.empty_grid)
+    puts "\nPick an empty cell player 0\n"
+    num2 = gets.chomp
+    player2.change_cell(num1, player2.matrix_grid, player2.empty_grid)
+  end
+
+  def show_empty_grid
+    puts '[" ", " ", " "]'
+    puts '[" ", " ", " "]'
+    puts '[" ", " ", " "]'
+  end
+
+  def pick_a_cell
+    puts "\nchoose a cell from 1-9 and then press enter"
   end
 end
 
@@ -81,22 +109,32 @@ class Player
   def empty_grid
     @@empty
   end
+
+  def self.class_grid
+    @@matrix
+  end
+
+  def self.instruction(player)
+    puts "The two teams are 'X' and 'O'"
+    puts "\nEach grid cell is labeled as so for you to choose\n"
+    # shows how each cell is labeled
+    puts player.display_grid(player.matrix_grid)
+    puts "\nPick an empty cell to begin \n"
+    player.show_empty_grid
+  end
 end
 
 first_player = Player.new('X')
+second_player = Player.new('0')
 
-second_player = Player.new('O')
+Player.instruction(first_player)
 
-puts "\nFirst player is team #{first_player.pick}"
-puts "\nSecond player is team #{second_player.pick}"
-puts "\nEach grid cell is labeled as so for you to choose"
+binding.break
 
-puts first_player.grid(first_player.matrix_grid)
+first_player.change_cell(gets.chomp.to_i, first_player.matrix_grid, first_player.empty_grid)
 
-puts "\nPick an empty cell to begin \n"
+#first_player.change_cell(1, first_player.matrix_grid, first_player.empty_grid)
 
-puts first_player.grid(first_player.empty_grid)
+#second_player.display_grid(second_player.empty_grid)
 
-first_player.change_cell(1, first_player.matrix_grid, first_player.empty_grid)
 
-puts second_player.grid(second_player.empty_grid)
